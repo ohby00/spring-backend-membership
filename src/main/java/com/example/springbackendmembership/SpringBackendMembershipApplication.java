48ef54a349b88bb898c5785e4437a2d38ff274a7
ohby00 <quddnr217@naver.com>
2022-11-27 15:53:09 +0900

    Initial commit

---
+++ src/main/java/com/example/springbackendmembership/SpringBackendMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbackendmembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendMembershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbackendmembership/SpringBackendMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbackendmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendMembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
